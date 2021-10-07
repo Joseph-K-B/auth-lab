c3170ba38926fcd4ff21367d91169cb8eb0aad75
Joseph-K-B <303josephb@gmail.com>
2021-10-07 13:14:00 -0700

    failing test to post user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const { response } = require('../lib/app.js');
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
@@ -10,5 +11,18 @@ describe('alchemy-app routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+});
+
+it('posts new user to DB', async () => {
+  const res = await request(app)
+    .post('/api/v1/signup')
+    .send({
+      email: 'test@email.com',
+      password: 'fake-password'
+    });
+  expect(res.body).toEqual({
+    id: expect.any(String),
+    email: 'test@email.com'
   });
 });
