9348a5e8e60d5b3896fa51f3ea3d0fddc531c84a
Joseph-K-B <303josephb@gmail.com>
2021-10-07 13:59:50 -0700

    post test passing but console log at controller displaying hash

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,27 +2,29 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
-const { response } = require('../lib/app.js');
+
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
+ 
+
+  it('posts new user to DB', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@email.com',
+        password: 'fake-password'
+      });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'test@email.com'
+    });
+  });
+
   afterAll(() => {
     pool.end();
-  });
-});
-
-it('posts new user to DB', async () => {
-  const res = await request(app)
-    .post('/api/v1/signup')
-    .send({
-      email: 'test@email.com',
-      password: 'fake-password'
-    });
-  expect(res.body).toEqual({
-    id: expect.any(String),
-    email: 'test@email.com'
   });
 });
